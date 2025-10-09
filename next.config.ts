ea526b0c0f18a994b29b4a9b5749f1f9b7fcece7
turt1e18 <pk0820@cu.ac.kr>
2025-10-09 18:11:43 +0900

    git fix and styled-component add

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   // images: {
   //   domains: ["placehold.co"]
   // }
+  compiler: {
+    styledComponents: true
+  },
   async redirects() {
     return [
       {
