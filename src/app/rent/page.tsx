69a933a78582ea1e4d99bfb46e5b6291acaa5d11
mongsil <wkdgustjrrpdla@naver.com>
2025-11-26 01:55:15 +0900

    대여페이지 제작

---
+++ src/app/rent/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+import Sidebar from "@/components/userMainPage/Sidebar";
+import Header from "@/components/userMainPage/Header";
+import RentalContent from "@/components/rent/RentalContent";
+import {
+  PageLayout,
+  ContentArea,
+  MainContainer,
+  MOCK_ITEMS
+} from "@/style/RentStyle";
+
+export default function RentPage() {
+  return (
+    <PageLayout>
+      <Sidebar currentPath="/rent" />
+      <ContentArea>
+        <Header />
+        <MainContainer>
+          <RentalContent items={MOCK_ITEMS} />
+        </MainContainer>
+      </ContentArea>
+    </PageLayout>
+  );
+}
